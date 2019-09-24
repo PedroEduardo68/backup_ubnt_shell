1fb3b5b858c0cfb00907d284b7569cb5a2be94dc
Pedro Camera <pedro.eduardo.camera@gmail.com>
2019-09-24 10:03:40 -0300

    initial commit

---
+++ backup_ubnt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#======== SEND BACKUP UBIQUITI FOR SSH      ==========#
+#=====================================================#
+#======== USER - PASSWORD - PORT            ==========#
+ssh_user="USER"                #USER                 
+ssh_pass="PASSWORD"            #PASSWORD
+ssh_porta="22"
+#=====================================================#
+# UBIQUITI COMMAND
+comando="cat /tmp/system.cfg"
+dia="`date +%d-%m-%Y`"
+#
+#RUN UBIQUITI COMMAND TO SSH
+for ubnt in $(cat /home/ftp_backup/ubiquiti/ubnt.txt); do
+ ssh admin@$ubnt "$comando" > /home/ftp_backup/ubiquiti/bkp-ubnt-$ubnt-$dia.cfg
+done
+#FILE END
